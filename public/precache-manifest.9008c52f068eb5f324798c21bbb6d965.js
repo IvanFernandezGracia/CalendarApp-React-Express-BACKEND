ec745c4b9db91a6fd4dcb56a52faf95294ba3f31
Ivan Alejandro <ivan.fernandez.g@usach.cl>
2022-03-02 22:34:32 -0300

    front react add prod build

---
+++ public/precache-manifest.9008c52f068eb5f324798c21bbb6d965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa39db821bb29baf27e73bd7ad157af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8812af4955ae9034f279",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "436b3e754f98a4e8c251",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "8812af4955ae9034f279",
+    "url": "/static/js/2.fac63192.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.fac63192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436b3e754f98a4e8c251",
+    "url": "/static/js/main.23a5c2a7.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);

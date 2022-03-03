9be2f69c3b583d5bbdb107964c713eb496a27237
Ivan Alejandro <ivan.fernandez.g@usach.cl>
2022-03-02 22:12:12 -0300

    init comit v1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CalendarApp-React-Express-BACKEND
+# Backend MERN - Calendar
+
+Backend que creamos en mi curso de React: de cero a experto
+
+fernando-herrera.com
---
+++ database/config.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  const username = encodeURIComponent(process.env.MONGO_USER);
+  const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+  const cluster = process.env.MONGO_CLUSTER;
+  const folder = process.env.MONGO_FOLDER;
+
+  const uri = `mongodb+srv://${username}:${password}@${cluster}/${folder}`;
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log("DB online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error a la hora de inicializar BD");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
